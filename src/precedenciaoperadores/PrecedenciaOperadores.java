ed7caaadec44c90c08240edec2f51fe6c528e1bc
auyon.j6356 <auyon.j6356@GTZ4LAPJULIOR.allied.global>
2018-03-06 18:01:40 -0600

    New commit

--- src/precedenciaoperadores/PrecedenciaOperadores.java
+++ src/precedenciaoperadores/PrecedenciaOperadores.java
@@ -17,6 +17,58 @@ public class PrecedenciaOperadores {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        System.out.println("Primero Ejemplo Precedencia de Operadores: ");
+        int x = 5;
+        int y = 10;
+        int z = ++x * y--;
+        System.out.println("x = " +x);
+        System.out.println("y = " +y);
+        System.out.println("z = " +z);
+        
+        System.out.println("Ejemplo Evaluacion: ");
+        System.out.println(4+5 * 6/3);
+        System.out.println((4+5) * (6/3));
+        
+        System.out.println("Otro Ejemplo Evaluacion: ");
+        System.out.println(1+2 - 3*4/5);
+        System.out.println(1+2 - (3*(4/5)));
+        
+        System.out.println("Otro Ejemplo: ");
+        //Si detecta una cadena, lo demas lo convierte a cadena
+        System.out.println("1 + 2 =" +1 +2);
+        //Los parentesis rompen esta regla ya que tienen mayor precedencia
+        System.out.println("1 + 2 = " + (1+2));
+        
+        System.out.println("\n Otro Ejemplo: ");
+        //El operador + es asociativo a la izquierda
+        System.out.println(1+2+"abc"); //Detecta una operacion primero 
+        System.out.println("abc" + 1+2);//Detecta una cadena primero
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+        
+        
         
         
     }
